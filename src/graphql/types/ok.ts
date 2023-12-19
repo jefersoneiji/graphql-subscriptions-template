099b42f57ef59fafe91f3ba54ae9b0c3ed27e9df
jefersoneiji <jeferson.eiji@icloud.com>
2023-12-19 12:15:28 -0300

    feat (graphql)!: added subscriptions to graphql

--- src/graphql/types/ok.ts
+++ src/graphql/types/ok.ts
@@ -20,3 +20,44 @@ export const OkQuery = extendType({
         });
     },
 });
+
+export const OkSubscription = extendType({
+    type: 'Subscription',
+    definition(t) {
+        t.nonNull.field('boolean', {
+            description: 'boolean field for testing subscriptions',
+            type: 'Boolean',
+            subscribe(root, args, ctx, info) {
+                return (async function* () {
+                    while (true) {
+                        await new Promise(res => setTimeout(res, 1000));
+                        yield Math.random() > .5;
+                    }
+                })();
+            },
+            resolve(eventData) {
+                return eventData;
+            },
+        });
+    },
+});
+
+export const GreetingsSubscriptions = extendType({
+    type: 'Subscription',
+    definition(t) {
+        t.nonNull.field('greetings', {
+            description: 'greetings field for testing subscriptions',
+            type: 'String',
+            subscribe(root, args, ctx, info) {
+                return (async function* () {
+                    for (const hi of ['Hi', 'Bonjour', 'Hola', 'Ciao', 'Zdravo']) {
+                        yield hi;
+                    }
+                })();
+            },
+            resolve(eventData) {
+                return eventData;
+            },
+        });
+    },
+});
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import Koa from 'koa';
 import cors from '@koa/cors';
 import Router from '@koa/router';
 import { graphqlHTTP } from 'koa-graphql';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
 
 import { schema } from './graphql/schema';
 import { context } from './graphql/context';
@@ -9,18 +11,26 @@ import { context } from './graphql/context';
 const app = new Koa();
 const router = new Router();
 
+const PORT = 4000;
 router.all('/graphql',
     graphqlHTTP({
         schema,
         context,
-        graphiql: true
+        graphiql: {
+            subscriptionEndpoint: `ws://localhost:${PORT}/graphql`,
+            websocketClient: 'graphql-ws',
+        }
     }));
 app
     .use(cors({ origin: '*' }))
     .use(router.routes())
     .use(router.allowedMethods());
 
-const PORT = 4000;
-app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
+    const wsServer = new WebSocketServer({
+        server,
+        path: '/graphql'
+    });
+    useServer({ schema }, wsServer);
     console.log(`Server running on http://localhost:${PORT}/graphql`);
 });
